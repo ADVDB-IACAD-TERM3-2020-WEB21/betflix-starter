06ec71e90a688e0f45cd28c3c80f54aff324e55b
John Paul Ada <johnpaulada@gmail.com>
2020-03-08 22:23:19 +0800

    feat: added cards

---
+++ script.js
@@ -0,0 +1,17 @@
+const getMovieList = () => {
+    return [{"_id":{"$oid":"5e630851836dbc1b0b0e91c9"},"title":"Castlevania","type":"series","genres":["Fantasy","Animated"],"videos":[{"title":"Ep 1","filename":"castlevania/s01e01.mp4"}]}, {"_id":{"$oid":"5e630851836dbc1b0b0e91c9"},"title":"Castlevania","type":"series","genres":["Fantasy","Animated"],"videos":[{"title":"Ep 1","filename":"castlevania/s01e01.mp4"}]}]
+}
+
+const movies = getMovieList()
+const movieListElement = document.querySelector("#movie-list")
+
+const moviesElements = movies.map(movie => {
+    const text = movie.title
+    const listItem = document.createElement('div')
+    listItem.classList.add("movie-container")
+    listItem.textContent = text
+
+    return listItem
+})
+
+movieListElement.append(...moviesElements)
