acaeb89bb6fb7477eae7fb8ab8608f0934bc7b6b
John Paul Ada <johnpaulada@gmail.com>
2020-03-14 10:18:14 +0800

    fix: remove models

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,11 +1,3 @@
-const mongoose = require("mongoose")
-
-const MONGO_CONNECTION_STRING = 'mongodb://localhost:27017/test'
-
-const { generateMovieModel } = require('./models/Movie')
-mongoose.connect(MONGO_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true});
-const Movie = generateMovieModel(mongoose)
-
 const getMovieList = async () => {
   // TODO: Get movies here
 }
